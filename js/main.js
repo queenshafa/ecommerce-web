ee31356dcd31645ab190b28b73c78346c19bd4b2
Queenshafa <queenshafaraniabilqis@gmail.com>
2026-02-17 10:42:53 +0700

    LAST COMMIT

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@ cards.forEach((card) => {
     bg.style.transform = `translateY(${cardTop}px)`;
   });
 });
+
+function toggleDrawer() {
+  const drawer = document.getElementById("cart-drawer");
+  const overlay = document.getElementById("cart-overlay");
+
+  if (drawer.classList.contains("translate-x-full")) {
+    // Open
+    drawer.classList.remove("translate-x-full");
+    overlay.classList.remove("hidden");
+    setTimeout(() => overlay.classList.add("opacity-100"), 10);
+    document.body.style.overflow = "hidden"; // Stop scrolling page
+  } else {
+    // Close
+    drawer.classList.add("translate-x-full");
+    overlay.classList.remove("opacity-100");
+    setTimeout(() => {
+      overlay.classList.add("hidden");
+      document.body.style.overflow = "auto";
+    }, 300);
+  }
+}
